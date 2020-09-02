9a06f10fb93ea0c6bf919798d33a754ff83d349e
nishantpratimdas <42237553+nishantpratimdas@users.noreply.github.com>
2020-09-02 14:51:16 +0530

    Update apps.lua

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,10 +8,10 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'env x-terminal-emulator -e /usr/bin/env TERM=xterm-256color byobu',
+    terminal = 'terminator',
     rofi = rofi_command,
     lock = 'i3lock-fancy',
-    quake = 'alacritty',
+    quake = 'terminator',
     screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
     region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
     delayed_screenshot = '~/.config/awesome/configuration/utils/screenshot --delayed -r',
